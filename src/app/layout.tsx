e3fb2c62968af6b1082c4d015f3cdb03eaceb112
Felipe Trevisan <felipejs@gmail.com>
2024-02-18 10:56:25 -0300

    feat: add new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Bebas_Neue } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const bebas = Bebas_Neue({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--bebas-font",
+});
+
+const neueRegular = localFont({
+  src: [{ path: "../../public/fonts/TT Firs Neue Trial Light.ttf", weight: "100"}, { path: "../../public/fonts/TT Firs Neue Trial Medium.ttf", weight: "400"}, { path: "../../public/fonts/TT Firs Neue Trial Bold.ttf", weight: "600"}],
+  variable: "--neue-font",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scrollbar scrollbar-thumb-secondary-foreground scrollbar-track-background">
-      <body className={`min-h-screen w-screen overflow-x-hidden ${inter.className}`}>{children}</body>
+    <html
+      lang="en"
+      className="scrollbar scrollbar-thumb-secondary-foreground scrollbar-track-background"
+    >
+      <body className={`min-h-screen w-screen overflow-x-hidden ${neueRegular.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
